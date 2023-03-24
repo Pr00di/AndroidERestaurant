bdf5d30f7e5609568a724805a76d5ad52e2c4012
Thomas <mailrt83-github@yahoo.com>
2023-03-24 11:10:56 +0100

    Vendredi_24_Mars

--- app/src/main/java/fr/isen/ravel/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/ravel/androiderestaurant/CategoryActivity.kt
@@ -1,35 +1,24 @@
 package fr.isen.ravel.androiderestaurant
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.toolbox.JsonObjectRequest
 import org.json.JSONObject
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
-import fr.isen.ravel.androiderestaurant.Data
-import fr.isen.ravel.androiderestaurant.Ingredients
-import fr.isen.ravel.androiderestaurant.Items
-import fr.isen.ravel.androiderestaurant.ListMenu
-import fr.isen.ravel.androiderestaurant.Prices
 
 class CategoryActivity : AppCompatActivity()
 {
     private lateinit var recyclerView: RecyclerView
     private lateinit var categoryAdapter: CategoryAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
 
@@ -40,33 +29,46 @@ class CategoryActivity : AppCompatActivity()
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         //
-        categoryAdapter = when (category) {
-            "Entrées" -> CategoryAdapter(resources.getStringArray(R.array.Entrees).toList(),object : CategoryAdapter.OnMenuItemClickListener
-            {
-                override fun onItemClick(itemsList:String){
-                    val intent = Intent(this@CategoryActivity, DetailActivity::class.java)
-                    intent.putExtra("itemsList", itemsList)
-                    startActivity(intent)}
-            })
+        categoryAdapter = when (category)
+        {
+            "Entrées" -> CategoryAdapter(emptyList() ,
+                object : CategoryAdapter.OnMenuItemClickListener
+                {
+                    override fun onItemClick(itemsList: String)
+                    {
+                        val intent = Intent(this@CategoryActivity , DetailActivity::class.java)
+                        intent.putExtra("itemsList" , itemsList)
+                        startActivity(intent)
+                    }
+                })
 
-            "Plats" -> CategoryAdapter(resources.getStringArray(R.array.Plats).toList(),object : CategoryAdapter.OnMenuItemClickListener
-            {
-                override fun onItemClick(itemsList:String){
-                    val intent = Intent(this@CategoryActivity, DetailActivity::class.java)
-                    intent.putExtra("itemsList", itemsList)
-                    startActivity(intent)}
-            })
+            "Plats" -> CategoryAdapter(emptyList() ,
+                object : CategoryAdapter.OnMenuItemClickListener
+                {
+                    override fun onItemClick(itemsList: String)
+                    {
+                        val intent = Intent(this@CategoryActivity , DetailActivity::class.java)
+                        intent.putExtra("itemsList" , itemsList)
+                        startActivity(intent)
+                    }
+                })
 
-            "Desserts" -> CategoryAdapter(resources.getStringArray(R.array.Desserts).toList(),object : CategoryAdapter.OnMenuItemClickListener
-            {
-                override fun onItemClick(itemsList:String){
-                    val intent = Intent(this@CategoryActivity, DetailActivity::class.java)
-                    intent.putExtra("itemsList", itemsList)
-                    startActivity(intent)}
-            })
+            "Desserts" -> CategoryAdapter(emptyList() ,
+                object : CategoryAdapter.OnMenuItemClickListener
+                {
+                    override fun onItemClick(itemsList: String)
+                    {
+                        val intent = Intent(this@CategoryActivity , DetailActivity::class.java)
+                        intent.putExtra("itemsList" , itemsList)
+                        startActivity(intent)
+                    }
+                })
 
-            else -> CategoryAdapter(emptyList(), object : CategoryAdapter.OnMenuItemClickListener {
-                override fun onItemClick(itemsList: String) {}
+            else -> CategoryAdapter(emptyList() , object : CategoryAdapter.OnMenuItemClickListener
+            {
+                override fun onItemClick(itemsList: String)
+                {
+                }
             })
         }
 
@@ -75,19 +77,19 @@ class CategoryActivity : AppCompatActivity()
         fetchMenu()
     }
 
-    private fun fetchMenu() {
+    private fun fetchMenu()
+    {
         val url = "http://test.api.catering.bluecodegames.com/menu"
         val requestBody = JSONObject().apply {
-            put("id_shop", 1)
+            put("id_shop" , 1)
         }
         val request = JsonObjectRequest(
-            Request.Method.POST, url, requestBody,
+            Request.Method.POST , url , requestBody ,
             { response ->
-                Log.d("ca marche", response.toString()) // Message dans le terminal de commande
+                Log.d("ca marche" , response.toString()) // Message dans le terminal de commande
 
                 // Je parse le JSON avec la librairie GSON afin d'obtenir des Listes de données claires
-                val gson = Gson()
-                val menuResult = gson.fromJson(response.toString(), ListMenu::class.java)
+                val menuResult = Gson().fromJson(response.toString() , ListMenu::class.java)
                 // Je viens recuperer la listMenu qui contient l'ensemble des données
 
                 // Je viens définir une variable qui va venir stocker la categorie selectionné
@@ -95,37 +97,33 @@ class CategoryActivity : AppCompatActivity()
 
                 // Je viens filtrer l'ensemble des données en piochant dans la partie data
                 // de la variable menuResult qui contient elle-meme la ListMenu (qui contient elle les data)
-                val filteredItems : List<String> = menuResult.data
+                val filteredItems: List<Items> = menuResult.data
                     .flatMap { it.items }
-                    .filter { item -> item.categNameFr == selectedCategoryName } // J'associe categNameFr avec selectedCategoryName
-                    .mapNotNull { menuItem -> menuItem.nameFr }
+                    .filter { item -> item.categNameFr == selectedCategoryName }
 
                 categoryAdapter.updateItems(filteredItems) // Je viens rafraichir l'adapteur pour qu'il m'affiche
                 // que les données comportant le bon cateNameFr
 
-
-                //val selectedCategory = menuResult.data.find { it.nameFr == selectedCategoryName }
-                // Filter menu items based on the selected category
-                //val selectedCategoryItems = menuResult.data.filter { it.nameFr == selectedCategoryName }
-
-                // Combine items from all categories with the same ID
-                //val combinedItems = selectedCategoryItems.flatMap { it.items }
-
-                // Update the adapter with the filtered menu items
-
-                /*val selectedCategory = menuResult.data.find { it.idCategory == selectedCategoryId }
-                // Update the adapter with the filtered menu items
-                selectedCategory?.let {
-                    val filteredItems: List<String> = it.items.mapNotNull { menuItem -> menuItem.nameFr }
-                    categoryAdapter.updateItems(filteredItems)
-                }*/
-            },
+            } ,
             { error ->
-                Log.d("404 error", error.toString())
+                Log.d("404 error" , error.toString())
             })
         Volley.newRequestQueue(this).add(request)
     }
-
-
 }
+
+    //val selectedCategory = menuResult.data.find { it.nameFr == selectedCategoryName }
+    // Filter menu items based on the selected category
+    //val selectedCategoryItems = menuResult.data.filter { it.nameFr == selectedCategoryName }
+
+    // Combine items from all categories with the same ID
+    //val combinedItems = selectedCategoryItems.flatMap { it.items }
+
+    // Update the adapter with the filtered menu items
+
+    /*val selectedCategory = menuResult.data.find { it.idCategory == selectedCategoryId }
+    // Update the adapter with the filtered menu items
+       selectedCategory?.let { val filteredItems: List<String> = it.items.mapNotNull { menuItem -> menuItem.nameFr }
+       categoryAdapter.updateItems(filteredItems) }*/
+
 
--- app/src/main/java/fr/isen/ravel/androiderestaurant/CategoryAdapter.kt
+++ app/src/main/java/fr/isen/ravel/androiderestaurant/CategoryAdapter.kt
@@ -1,23 +1,24 @@
 package fr.isen.ravel.androiderestaurant
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ActionMenuView.OnMenuItemClickListener
-import android.widget.AdapterView.OnItemClickListener
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 
-class CategoryAdapter(private var itemsList : List<String> , private val onMenuItemClickListener: OnMenuItemClickListener) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>()
+class CategoryAdapter(private var itemsList : List<Items> , private val onMenuItemClickListener: OnMenuItemClickListener) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>()
 {
     // Les : permettent l'heritage
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
         val textViewItem: TextView = itemView.findViewById(R.id.textViewItem)
+        val imageViewItem: ImageView = itemView.findViewById(R.id.imageView) // Ajoutez un ImageView dans votre fichier de mise en page d'élément de liste
+        //val PriceViewItem: TextView = itemView.findViewById(R.id.PriceItem)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup , viewType: Int): ViewHolder
@@ -29,10 +30,23 @@ class CategoryAdapter(private var itemsList : List<String> , private val onMenuI
     override fun onBindViewHolder(holder: ViewHolder , position: Int)
     {   // pareil que onCreateViewHolder appeler autant de fois qu'il y a d'item a representer
         // dans la liste courante
-        holder.textViewItem.text = itemsList[position]
-        holder.itemView.setOnClickListener{
-             onMenuItemClickListener.onItemClick(itemsList[position])
+        //holder.textViewItem.text = itemsList[position]
+        val menuItem = itemsList[position]
+
+        val imageUrl = itemsList[position].images.firstOrNull() // Remplacez cette ligne par la logique d'obtention de l'URL de l'image à partir de vos données
+        if (!imageUrl.isNullOrEmpty()) { // le contraire de si elle nul ou vide
+            Picasso.get().load(imageUrl).into(holder.imageViewItem) // permet d'afficher les images
+        } else {
+           // Picasso.
+            // Affichez une image par défaut ou masquez l'ImageView si aucune image n'est disponible
         }
+
+        holder.textViewItem.text = menuItem.nameFr
+        holder.itemView.setOnClickListener{
+             onMenuItemClickListener.onItemClick(menuItem.nameFr ?: "")
+        }
+
+        // Charger l'image en utilisant Picasso
         //val dish = dishes[position]
         //holder.dishName.text = dish
         //holder.itemView.setOnClickListener
@@ -45,13 +59,16 @@ class CategoryAdapter(private var itemsList : List<String> , private val onMenuI
     }
 
     interface OnMenuItemClickListener{
-        fun onItemClick(itemsList: String)
+        fun onItemClick(item: String)
     }
 
-    fun updateItems(newItemsList: List<String>) {
+    fun updateItems(newItemsList: List<Items>) {
         itemsList = newItemsList
         notifyDataSetChanged()
     }
+
+    //  Le CategoryAdapter utilise maintenant des objets Items
+    //  au lieu de simples chaînes de caractères pour afficher les informations sur les éléments du menu.
 
     //fun updateDishes(dishesFromAPI:ArrayList<String>) {
     // dishes = dishesFromAPI
