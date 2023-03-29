55baaff3170c201f30977fbf0ca0dc23648a9301
Thomas <mailrt83-github@yahoo.com>
2023-03-29 19:07:51 +0200

    Mercredi_29_Mars_Bouton+-_Prix

--- app/src/main/java/fr/isen/ravel/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/ravel/androiderestaurant/DetailActivity.kt
@@ -8,69 +8,74 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
-import com.google.gson.Gson
 import com.squareup.picasso.Picasso
-import java.lang.StringBuilder
+import java.text.DecimalFormat
 
 @Suppress("DEPRECATION")
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var itemName: Items
+    // Initialiser les variables
+    private var quantity = 1
+    var price = 0.0// Remplacez cette valeur par le prix du plat
+    private lateinit var priceTextView: Button
+
+    // Mettre à jour le prix en fonction de la quantité
+    private fun updatePrice() {
+        val totalPrice = price * quantity.toDouble()
+        val decimalFormat = DecimalFormat("#.00")
+        priceTextView.text = "Prix: ${decimalFormat.format(totalPrice)} €"
+    }
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val itemName = intent.getSerializableExtra("itemsList") as Items
-        Log.d("DetailActivity", "itemSelectionne: $itemName")
+        itemName = intent.getSerializableExtra("itemsList") as Items
+        Log.d("DetailActivity" , "itemSelectionne: $itemName")
+        //price = itemName.prices[0].price.
 
-        // Les éléments à afficher lors de la sélection
+        // Récupérer les éléments de la vue
         val imageViewPager = findViewById<ViewPager>(R.id.imageViewPager)
-        val imageAdapter = ImagePagerAdapter(supportFragmentManager, itemName.images)
+        val imageAdapter = ImagePagerAdapter(supportFragmentManager , itemName.images)
         imageViewPager.adapter = imageAdapter
-
         val nameView = findViewById<TextView>(R.id.NamePlats)
-        val descriptionView = findViewById<TextView>(R.id.DescriptionPlats)
-        //val priceView = findViewById<TextView>(R.id.textViewPrice)
-
-        /*if(itemName.images.isNotEmpty() && itemName.images[0].isNotEmpty()){
-            Picasso.get()
-                .load(itemName.images[0])
-                .placeholder(R.drawable.placeholder_image)
-                .error(R.drawable.placeholder_image)
-                .into(imageViewPager)
-        } else{
-            imageViewPager.setPageMarginDrawable(R.drawable.placeholder_image)
-        }*/
+        val descriptionView  = findViewById<TextView>(R.id.DescriptionPlats)
+        val decreaseButton = findViewById<Button>(R.id.decreaseButton)
+        val increaseButton = findViewById<Button>(R.id.increaseButton)
+        val quantityTextView = findViewById<TextView>(R.id.QuantitySelected)
+        priceTextView = findViewById<Button>(R.id.buttonPrice)
 
         //On met à jour les infos de la selection
         nameView.text = itemName.nameFr
-        descriptionView.text = itemName.ingredients.joinToString(",") { it.nameFr.orEmpty()}
+        descriptionView.text = itemName.ingredients.joinToString(",") { it.nameFr.orEmpty() }
 
-        /*
-        val IngredientsList = itemName.ingredients
-        val sb = StringBuilder()
-        for (i in IngredientsList.indices)
-        {
-            sb.append(IngredientsList[i].nameFr)
-            if (i != IngredientsList.size - 1)
-            {
-                sb.append(", ")
+        // Gérer les événements de clic sur les boutons
+        decreaseButton.setOnClickListener {
+            if (quantity > 1) {
+                quantity -= 1
+                quantityTextView.text = quantity.toString()
+                updatePrice()
             }
         }
-        descriptionView.text = sb.toString()
-        //priceView.text = itemName.prices[0].price
-        //Picasso.get().load(itemName.images).into(imageView)
-        //descriptionView.text = itemNameJson.ingredients
-        //prixPlat.text = String.format(Locale.getDefault(), "%.2f", plat.prix)
-        //val item = gson.fromJson(itemJson , Items::class.java)
 
-        val itemImageJson = intent.getSerializableExtra("itemsList") as Items
-        */
+        increaseButton.setOnClickListener {
+            if (quantity < 10) {
+                quantity += 1
+                quantityTextView.text = quantity.toString()
+                updatePrice()
+            }
+        }
+
+        // Initialiser le prix
+        updatePrice()
     }
 
 
@@ -84,7 +89,11 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-
+    //L'imageFragment est une classe de fragment personnalisée qui va permettre d'afficher
+    //une image à l'intérieur d'un ViewPager
+    //Dans notre cas, on veut afficher une image à partir d'une liste d'URL d'images.
+    //Le ViewPager utilisera plusieurs instances de ce fragment pour afficher chaque image de la liste d'URL
+    //permettant à l'utilisateur de faire défiler les images horizontalement.
     class ImageFragment : Fragment()
     {
 
